88e13b4597cb07a42bb2b46b337bdd268872298a
Prashuk Jain <148011967+PrashukJain29@users.noreply.github.com>
2026-06-10 07:45:40 +0000

    implement backend check for token with logger import

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
+import sys
+
 
 from services.home_activities import *
 from services.notifications_activities import *
@@ -13,6 +15,7 @@ from services.message_groups import *
 from services.messages import *
 from services.create_message import *
 from services.show_activity import *
+from lib.cognito_jwt_token import CognitoJwtToken, extract_access_token, TokenVerifyError
 
 #HoneyComb ----------------
 from opentelemetry import trace
@@ -25,6 +28,7 @@ from opentelemetry.sdk.trace.export import SimpleSpanProcessor, ConsoleSpanExpor
 
 import watchtower
 import logging
+from logging import StreamHandler
 from time import strftime
 #ROLLBAR ----------------
 import os
@@ -49,21 +53,44 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 #HoneyComb ----------------
 # Initialize tracing and an exporter that can send data to Honeycomb
-provider = TracerProvider()
-processor = BatchSpanProcessor(OTLPSpanExporter())
-simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(processor)
-provider.add_span_processor(simple_processor)
-trace.set_tracer_provider(provider)
-tracer = trace.get_tracer(__name__)
+#provider = TracerProvider()
+#processor = BatchSpanProcessor(OTLPSpanExporter())
+#simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+#provider.add_span_processor(processor)
+#provider.add_span_processor(simple_processor)
+#trace.set_tracer_provider(provider)
+#tracer = trace.get_tracer(__name__)
 
 
 app = Flask(__name__)
+
+cognito_jwt_token = CognitoJwtToken(
+  user_pool_id=os.getenv("AWS_COGNITO_USER_POOL_ID"), 
+  user_pool_client_id=os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+  region=os.getenv("AWS_DEFAULT_REGION")
+)
+
+# Create logger
+logger = logging.getLogger('flask.app')
+logger.setLevel(logging.DEBUG)
+
+# Console handler
+handler = StreamHandler()
+handler.setLevel(logging.DEBUG)
+
+# Formatter
+formatter = logging.Formatter('[%(asctime)s] %(levelname)s in %(name)s: %(message)s')
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+app.logger = logger
+
+app.debug = True
 #XRayMiddleware(app, xray_recorder)
 #HoneyComb ----------------
 # Initialize automatic instrumentation with Flask
-FlaskInstrumentor().instrument_app(app)
-RequestsInstrumentor().instrument()
+#FlaskInstrumentor().instrument_app(app)
+#RequestsInstrumentor().instrument()
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
@@ -71,8 +98,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -148,8 +175,21 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  access_token = extract_access_token(request.headers)
+  try:
+    claims = cognito_jwt_token.verify(access_token)
+    # authenicatied request
+    app.logger.debug("authenicated")
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
+  except TokenVerifyError as e:
+    # unauthenicatied request
+    app.logger.debug(e)
+    app.logger.debug("unauthenicated")
+    data = HomeActivities.run()
   return data, 200
+
 
 @app.route("/api/activities/notifications", methods=['GET'])
 def data_notifications():
